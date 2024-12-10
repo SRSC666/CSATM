2aec55c6b4a182d117897dab17493d2f4e86b3b6
SRSC <3879937469@qq.com>
2024-12-10 23:01:43 +0800

    Add comments for ATM and bank

--- Backend/Bank.cs
+++ Backend/Bank.cs
@@ -2,8 +2,14 @@
 {
     public class Bank(string name)
     {
+        /// <summary>
+        /// 银行名称
+        /// </summary>
         public string Name { get; set; } = name;
 
+        /// <summary>
+        /// 账户表
+        /// </summary>
         public List<Account> Accounts { get; } = [];
     }
 }
