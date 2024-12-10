0e56fe55ef0eab06007c4742aaed6baa1c4cc35a
SRSC <3879937469@qq.com>
2024-12-10 23:13:56 +0800

    Create Result.cs

---
+++ Backend/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace CSATM.Backend
+{
+    /// <summary>
+    /// 操作结果
+    /// </summary>
+    public class Result(int code, string message, object data)
+    {
+        public int Code { get; } = code;
+        public string Message { get; } = message;
+        public object Data { get; } = data;
+
+        public static Result Success() => new(1, "Success", null);
+        public static Result Success(object data) => new(1, "Success", data);
+        public static Result Failure(string message) => new(0, message, null);
+    }
+}
