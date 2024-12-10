6b99e2203b18b283a82b1ad4ee237c316418df9b
SRSC <3879937469@qq.com>
2024-12-10 22:58:39 +0800

    Add comments for Account

--- Backend/Account.cs
+++ Backend/Account.cs
@@ -2,12 +2,27 @@
 {
     public class Account(string bankName, string number, string username, string password)
     {
+        /// <summary>
+        /// 银行名
+        /// </summary>
         public string BankName { get; } = bankName;
+        /// <summary>
+        /// 卡号
+        /// </summary>
         public string Number { get; } = number;
 
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
         public string Username { get; set; } = username;
+        /// <summary>
+        /// 密码
+        /// </summary>
         public string Password { get; set; } = password;
 
+        /// <summary>
+        /// 余额
+        /// </summary>
         public decimal Balance { get; set; }
     }
 }
