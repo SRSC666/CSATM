cabb54ea8450e946b8658d4b9f9dbdcf0e7dd231
SRSC <3879937469@qq.com>
2024-12-10 22:55:53 +0800

    Add load and save bank data method

--- Backend/ATM.cs
+++ Backend/ATM.cs
@@ -2,8 +2,20 @@
 {
     public static class ATM
     {
+        public const string FileName = "data.json";
+
         public static List<Bank> Banks { get; } = [];
 
         public static Account CurrentAccount { get; set; }
+
+        public static void LoadBankData()
+        {
+            
+        }
+
+        public static void SaveBankData()
+        {
+
+        }
     }
 }
