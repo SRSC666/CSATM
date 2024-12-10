19b7d409f85ca391e9345da4877c32ac8e01a454
SRSC <3879937469@qq.com>
2024-12-10 21:29:58 +0800

    Create Account.cs

---
+++ Backend/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace CSATM.Backend
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string Password { get; set; }
+    }
+}
