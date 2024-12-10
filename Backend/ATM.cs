32684c5d65d35b39f97fab165d9d6339c6eb4c45
SRSC <3879937469@qq.com>
2024-12-10 23:05:01 +0800

    Move some properties from ATM to Data

--- Backend/ATM.cs
+++ Backend/ATM.cs
@@ -1,36 +1,10 @@
 ﻿namespace CSATM.Backend
 {
-    public static class ATM
+    public class ATM
     {
-        /// <summary>
-        /// 数据存储文件名
-        /// </summary>
-        public const string FileName = "data.json";
-
-        /// <summary>
-        /// 银行表
-        /// </summary>
-        public static List<Bank> Banks { get; } = [];
-
         /// <summary>
         /// 当前操作的账户
         /// </summary>
         public static Account CurrentAccount { get; set; }
-
-        /// <summary>
-        /// 读取银行数据
-        /// </summary>
-        public static void LoadBankData()
-        {
-            
-        }
-
-        /// <summary>
-        /// 保存银行数据
-        /// </summary>
-        public static void SaveBankData()
-        {
-
-        }
     }
 }
--- Backend/Data.cs
+++ Backend/Data.cs
@@ -2,5 +2,30 @@
 {
     public class Data
     {
+        /// <summary>
+        /// 数据存储文件名
+        /// </summary>
+        public const string FileName = "data.json";
+
+        /// <summary>
+        /// 银行表
+        /// </summary>
+        public static List<Bank> Banks { get; } = [];
+
+        /// <summary>
+        /// 读取银行数据
+        /// </summary>
+        public static void LoadBankData()
+        {
+
+        }
+
+        /// <summary>
+        /// 保存银行数据
+        /// </summary>
+        public static void SaveBankData()
+        {
+
+        }
     }
 }
