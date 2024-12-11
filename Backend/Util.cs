a0adbf7cf49d7b2728f615a80ac7b7b123d371d8
SRSC <3879937469@qq.com>
2024-12-11 14:42:58 +0800

    Create Util.cs

---
+++ Backend/Util.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CSATM.Backend
+{
+    public class Util
+    {
+        public static string ToMD5(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
