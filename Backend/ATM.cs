9420582b2bd23ca4f5e30cae86a085e5fe8aa066
SRSC <3879937469@qq.com>
2024-12-11 10:34:33 +0800

    Login

--- Backend/ATM.cs
+++ Backend/ATM.cs
@@ -9,5 +9,29 @@
         /// 当前操作的账户
         /// </summary>
         public static Account CurrentAccount { get; set; }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="bankName"></param>
+        /// <param name="number"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static Result Login(string bankName, string number, string password)
+        {
+            Account account = Data.GetAccount(bankName, number);
+            if (account == null)
+            {
+                return Result.Failure("该账户不存在");
+            }
+
+            if (password != account.Password)
+            {
+                return Result.Failure("密码错误");
+            }
+
+            CurrentAccount = account;
+            return Result.Success();
+        }
     }
 }
