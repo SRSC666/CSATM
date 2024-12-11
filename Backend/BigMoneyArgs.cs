ed9c77d61d7efe4512006f1deb94a7d1ed8a6209
SRSC <3879937469@qq.com>
2024-12-11 11:20:28 +0800

    Create BigMoneyArgs.cs

---
+++ Backend/BigMoneyArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace CSATM.Backend
+{
+    public class BigMoneyArgs(string accountNumber, decimal amount)
+    {
+        public string AccountNumber { get; set; } = accountNumber;
+        public decimal Amount { get; set; } = amount;
+    }
+}
